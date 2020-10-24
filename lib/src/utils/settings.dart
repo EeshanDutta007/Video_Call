5d3f42a71e24e1e53d8f0ca45d00c46294166459
Eeshan Dutta <eeshandutta21@gmail.com>
2020-10-25 03:54:35 +0530

    Basic structure added

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = "d2320273ebfc498c9dda1b61602bf17e";
+const Token =
+    "006ec6ff8430be942e582f0bc85adb15b5bIADvck7K8jDBiH+1OxzBfrrGSvWeENWlZPs6/dtnQQHPcMJBJDUAAAAAEAAi3E9l4fmVXwEAAQDh+ZVf";
